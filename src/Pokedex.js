84b9d41f533394d17b8f4016687b15834589472f
dakadabra <37588608+dakadabra@users.noreply.github.com>
2020-01-11 17:26:01 -0500

    Hardcode a few pokemon

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import { PokemonType } from "./PokemonType";
 
 export function Pokedex() {
   const pokemon = {
-    //Object
+    //object of pokemon
     Squirtle: { type: "water" },
     Bulbasaur: { type: "grass" },
     Charmander: { type: "fire" }
   };
 
+  const [selectedPokemon, setSelectedPokemon] = useState("Bulbasaur");
+
   return (
     <div>
-      <h4>Squirtle</h4>
+      <div>
+        <h4>{selectedPokemon}</h4>
 
-      <PokemonType type={"water"} />
+        <PokemonType type={pokemon[selectedPokemon].type} />
+      </div>
+
+      <div>
+        <p>Select your pokemon!</p>
+        <button onClick={() => setSelectedPokemon("Squirtle")}>Squirtle</button>
+        <button onClick={() => setSelectedPokemon("Bulbasaur")}>Bulbasaur</button>
+        <button onClick={() => setSelectedPokemon("Charmander")}>Charmander</button>
+      </div>
     </div>
+    
   );
 }
+
