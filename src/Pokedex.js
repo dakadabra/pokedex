bd99430bb34fceea033fe4f43eefbc7798196a43
dakadabra <37588608+dakadabra@users.noreply.github.com>
2020-01-11 17:50:35 -0500

    Added the PokeAPI

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,31 +2,36 @@ import React, {useState} from "react";
 import { PokemonType } from "./PokemonType";
 
 export function Pokedex() {
-  const pokemon = {
-    //object of pokemon
-    Squirtle: { type: "water" },
-    Bulbasaur: { type: "grass" },
-    Charmander: { type: "fire" }
-  };
 
-  const [selectedPokemon, setSelectedPokemon] = useState("Bulbasaur");
+  const [selectedPokemon, setSelectedPokemon] = useState(undefined);
+
+
+  //async means run at same time as rest of code
+  async function getPokemonInfo(name) {
+    const URL = `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}``;
+
+    const response = await fetch(URL); // wait to get data
+    const data = await response.json();
+    setSelectedPokemon(data);
+  }
 
   return (
     <div>
-      <div>
-        <h4>{selectedPokemon}</h4>
+      {/* if selectedPokemon isn't undefined */}
+      {selectedPokemon && (
+        <div>
+          <h4>{selectedPokemon.species.name}</h4>
 
-        <PokemonType type={pokemon[selectedPokemon].type} />
-      </div>
-
+          <PokemonType type={selectedPokemon.types[0].type.name} />
+        </div>
+      )}
       <div>
         <p>Select your pokemon!</p>
-        <button onClick={() => setSelectedPokemon("Squirtle")}>Squirtle</button>
-        <button onClick={() => setSelectedPokemon("Bulbasaur")}>Bulbasaur</button>
-        <button onClick={() => setSelectedPokemon("Charmander")}>Charmander</button>
+        <button onClick={() => getPokemonInfo("Squirtle")}>Squirtle</button>
+        <button onClick={() => getPokemonInfo("Bulbasaur")}>Bulbasaur</button>
+        <button onClick={() => getPokemonInfo("Charmander")}>Charmander</button>
       </div>
     </div>
-    
+
   );
 }
-
